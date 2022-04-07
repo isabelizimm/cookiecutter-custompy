d81a993bfe2a4172fdb3495bf1aa59d4fec08fe6
isabelizimm <isabel.zimmerman@rstudio.com>
2022-04-07 14:02:59 -0400

    remove codecov, requires user input

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -1,13 +1,9 @@
-"""Tests for `{{ cookiecutter.project_slug }}` package."""
-
 {% if cookiecutter.use_pytest == 'y' -%}
 import pytest
 {%- endif %}
 
 
-def test_setup(self):
+def test_setup():
     """Set up test fixtures, if any."""
-
-def test_teardown(self):
-    """Tear down test fixtures, if any."""
+    assert "mock" == "mock"
 
